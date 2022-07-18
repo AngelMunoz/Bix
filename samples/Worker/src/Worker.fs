9b61df28f1c1f69b89e44dc05701b4da752d3112
Angel D. Munoz <scythes.lol@gmail.com>
2022-07-18 13:36:14 -0500

    Add Cloudflare worker sample

---
+++ samples/Worker/src/Worker.fs
@@ -0,0 +1,19 @@
+﻿module Worker
+
+open Bix
+open Bix.Router
+open Bix.Handlers
+
+open Bix.Cloudflare
+
+
+let routes =
+    Router.Empty
+    |> Router.get ("/", sendHtml "<h1>Hello, World!</h1>")
+    |> Router.get ("/text", sendText "Hello World!")
+    |> Router.get ("/json", sendJson {| message = "Hello, World!" |})
+
+let worker = Worker.Empty |> Worker.withRouter routes |> Worker.build
+
+// ES Modules need to export the fetch handler as per cloudflare documentation
+Fable.Core.JsInterop.exportDefault worker
