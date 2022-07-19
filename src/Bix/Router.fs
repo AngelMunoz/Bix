e32edfca4f14d0bb9ebf282bb6fe0635db35ec28
Angel D. Munoz <scythes.lol@gmail.com>
2022-07-19 00:17:18 -0500

    add saturn-like router

--- src/Bix/Router.fs
+++ src/Bix/Router.fs
@@ -1,13 +1,15 @@
 module Bix.Router
 
 open URLPattern
+open Fable.Core
 open Bix.Types
 
 type Router =
 
     static member inline Empty: list<RouteDefinition> = List.empty
+
     static member inline get
-        (path: string, handler: HttpHandler)
+        (path: string, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Get
@@ -16,7 +18,7 @@ type Router =
         :: routes
 
     static member inline post
-        (path: string, handler: HttpHandler)
+        (path: string, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Post
@@ -25,7 +27,7 @@ type Router =
         :: routes
 
     static member inline put
-        (path: string, handler: HttpHandler)
+        (path: string, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Put
@@ -34,7 +36,7 @@ type Router =
         :: routes
 
     static member inline delete
-        (path: string, handler: HttpHandler)
+        (path: string, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Delete
@@ -43,7 +45,7 @@ type Router =
         :: routes
 
     static member inline patch
-        (path: string, handler: HttpHandler)
+        (path: string, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Patch
@@ -52,7 +54,7 @@ type Router =
         :: routes
 
     static member inline head
-        (path: string, handler: HttpHandler)
+        (path: string, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Head
@@ -61,7 +63,7 @@ type Router =
         :: routes
 
     static member inline all
-        (path: string, handler: HttpHandler)
+        (path: string, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = All
@@ -70,7 +72,7 @@ type Router =
         :: routes
 
     static member inline custom
-        (method: string, path: string, handler: HttpHandler)
+        (method: string, path: string, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Custom method
@@ -79,7 +81,7 @@ type Router =
         :: routes
 
     static member inline getP
-        (patternArgs: UrlInitArgs list, handler: HttpHandler)
+        (patternArgs: UrlInitArgs list, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Get
@@ -89,7 +91,7 @@ type Router =
 
 
     static member inline postP
-        (patternArgs: UrlInitArgs list, handler: HttpHandler)
+        (patternArgs: UrlInitArgs list, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Post
@@ -98,7 +100,7 @@ type Router =
         :: routes
 
     static member inline putP
-        (patternArgs: UrlInitArgs list, handler: HttpHandler)
+        (patternArgs: UrlInitArgs list, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Put
@@ -107,7 +109,7 @@ type Router =
         :: routes
 
     static member inline deleteP
-        (patternArgs: UrlInitArgs list, handler: HttpHandler)
+        (patternArgs: UrlInitArgs list, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Delete
@@ -116,7 +118,7 @@ type Router =
         :: routes
 
     static member inline patchP
-        (patternArgs: UrlInitArgs list, handler: HttpHandler)
+        (patternArgs: UrlInitArgs list, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Patch
@@ -125,7 +127,7 @@ type Router =
         :: routes
 
     static member inline headP
-        (patternArgs: UrlInitArgs list, handler: HttpHandler)
+        (patternArgs: UrlInitArgs list, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Head
@@ -134,7 +136,7 @@ type Router =
         :: routes
 
     static member inline allP
-        (patternArgs: UrlInitArgs list, handler: HttpHandler)
+        (patternArgs: UrlInitArgs list, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = All
@@ -143,10 +145,143 @@ type Router =
         :: routes
 
     static member inline customP
-        (method: string, patternArgs: UrlInitArgs list, handler: HttpHandler)
+        (method: string, patternArgs: UrlInitArgs list, [<InlineIfLambda>] handler: HttpHandler)
         (routes: RouteDefinition list)
         : RouteDefinition list =
         { method = Custom method
           pattern = unbox (patternArgs |> UrlPatternInit.fromArgs)
           handler = handler }
         :: routes
+
+let inline itemsHandler (handler: HttpContext -> JS.Promise<BixResponse>) (next: HttpFunc) (ctx: HttpContext) =
+    handler ctx
+    |> Promise.map (fun bixRes ->
+        ctx.SetStarted true
+        Some bixRes)
+
+let inline singleItemhandler
+    (handler: string option -> HttpContext -> JS.Promise<BixResponse>)
+    (next: HttpFunc)
+    (ctx: HttpContext)
+    =
+    handler (ctx.PathParams "id") ctx
+    |> Promise.map (fun bixRes ->
+        ctx.SetStarted true
+        Some bixRes)
+
+type ControllerBuilder() =
+    member inline _.Yield _ = []
+
+    [<CustomOperation("find")>]
+    member inline _.Find(state, [<InlineIfLambda>] handler: HttpContext -> JS.Promise<BixResponse>) =
+
+        { method = Get
+          pattern = unbox ""
+          handler = itemsHandler handler }
+        :: state
+
+    [<CustomOperation("findOne")>]
+    member inline _.FindOne
+        (
+            state,
+            [<InlineIfLambda>] handler: string option -> HttpContext -> JS.Promise<BixResponse>
+        ) =
+        { method = Get
+          pattern = unbox "/:id"
+          handler = singleItemhandler handler }
+        :: state
+
+    [<CustomOperation("create")>]
+    member inline _.Create(state, [<InlineIfLambda>] handler: HttpContext -> JS.Promise<BixResponse>) =
+        { method = Post
+          pattern = unbox ""
+          handler = itemsHandler handler }
+        :: state
+
+    [<CustomOperation("update")>]
+    member inline _.Update(state, [<InlineIfLambda>] handler: HttpContext -> JS.Promise<BixResponse>) =
+        { method = Put
+          pattern = unbox ""
+          handler = itemsHandler handler }
+        :: state
+
+    [<CustomOperation("updateOne")>]
+    member inline _.UpdateOne
+        (
+            state,
+            [<InlineIfLambda>] handler: string option -> HttpContext -> JS.Promise<BixResponse>
+        ) =
+        { method = Put
+          pattern = unbox "/:id"
+          handler = singleItemhandler handler }
+        :: state
+
+    [<CustomOperation("delete")>]
+    member inline _.Delete(state, [<InlineIfLambda>] handler: string option -> HttpContext -> JS.Promise<BixResponse>) =
+        { method = Delete
+          pattern = unbox "/:id"
+          handler = singleItemhandler handler }
+        :: state
+
+    member inline _.Run(state) : RouteDefinition list = state
+
+type RouterBuilder() =
+
+    member inline _.Yield _ = []
+
+    [<CustomOperation("get")>]
+    member inline _.Get(state, path: string, [<InlineIfLambda>] handler: HttpHandler) =
+        { method = Get
+          pattern = unbox path
+          handler = handler }
+        :: state
+
+    [<CustomOperation("post")>]
+    member inline _.Post(state, path: string, [<InlineIfLambda>] handler: HttpHandler) =
+        { method = Post
+          pattern = unbox path
+          handler = handler }
+        :: state
+
+    [<CustomOperation("put")>]
+    member inline _.Put(state, path: string, [<InlineIfLambda>] handler: HttpHandler) =
+        { method = Put
+          pattern = unbox path
+          handler = handler }
+        :: state
+
+    [<CustomOperation("patch")>]
+    member inline _.Patch(state, path: string, [<InlineIfLambda>] handler: HttpHandler) =
+        { method = Patch
+          pattern = unbox path
+          handler = handler }
+        :: state
+
+    [<CustomOperation("delete")>]
+    member inline _.Delete(state, path: string, [<InlineIfLambda>] handler: HttpHandler) =
+        { method = Delete
+          pattern = unbox path
+          handler = handler }
+        :: state
+
+    [<CustomOperation("custom")>]
+    member inline _.Delete(state, method: string, path: string, [<InlineIfLambda>] handler: HttpHandler) =
+        { method = Custom method
+          pattern = unbox path
+          handler = handler }
+        :: state
+
+    [<CustomOperation("forward")>]
+    member inline _.Forward(state: RouteDefinition list, url: string, routes: RouteDefinition list) =
+
+        let routes =
+            routes
+            |> List.map (fun r -> { r with pattern = unbox ($"{url}{r.pattern}") })
+            |> List.append state
+
+        routes
+
+    member inline _.Run(state) : RouteDefinition list = state
+
+let router = RouterBuilder()
+let controller = ControllerBuilder()
